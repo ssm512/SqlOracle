960e350a71d00f94bc2245000d6846f9cf88a31c
ssm512 <shinsang512@naver.com>
2026-04-06 14:27:17 +0900

    20260406 sql 3rd commit

--- hr20260406.sql
+++ hr20260406.sql
@@ -29,14 +29,19 @@ SELECT      EMPLOYEE_ID, FIRST_NAME, SALARY
  SELECT     EMPLOYEE_ID, FIRST_NAME, PHONE_NUMBER
     FROM    EMPLOYEES
     WHERE   PHONE_NUMBER LIKE '%010%'
-    ORDER BY PHONE_NUMBER DESC;
+    ORDER BY EMPLOYEE_ID ASC;
   
  -- 50번 부서의 직원을출력해라
- SELECT     EMPLOYEE_ID, FIRST_NAME, LAST_NAME
+ SELECT     EMPLOYEE_ID                      사번,        -- 사번 : Alias 별칭,별명 -> 자바에서는 Alias 명으로 db에서 data를 받아옴
+            FIRST_NAME || ' ' || LAST_NAME   이름,     -- Alias 안에 띄워쓰기를 포함하고 싶으면 ""를 사용하여 둘러싸면 됨
+            DEPARTMENT_ID                    "부서 번호"
     FROM    EMPLOYEES
-    WHERE   DEPARTMENT_ID = 50; 
+    WHERE   DEPARTMENT_ID = 50
+    --ORDER BY FIRST_NAME || ' ' || LAST_NAME ASC;
+    ORDER BY FIRST_NAME ASC, LAST_NAME ASC; -- FIRST_NAME으로 1차 정렬, LAST_NAME으로 2차 정렬
     
  -- 부서가 없는 직원을 출력
-  SELECT     EMPLOYEE_ID, FIRST_NAME, LAST_NAME, DEPARTMENT_ID
+  SELECT    EMPLOYEE_ID, FIRST_NAME, LAST_NAME, DEPARTMENT_ID
     FROM    EMPLOYEES
     WHERE   DEPARTMENT_ID IS NULL;
+    ORDER BY EMPLOYEE_ID ASC;
