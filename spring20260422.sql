b56450abf120bf39652d80cb1ff1f687a85bd0ae
ssm512 <shinsang512@naver.com>
2026-05-14 17:47:38 +0900

    20260514 board7 sql commit

--- spring20260422.sql
+++ spring20260422.sql
@@ -400,4 +400,44 @@ AND
 
 COMMIT;
 
+------------------------------------------------------------------------
+20260514
+-- 멀티게시판 자료실
+
+CREATE TABLE FILES
+(
+    FILE_NUM    NUMBER(6,0) NOT NULL    --파일고유번호
+    , IDX       NUMBER(6,0) NOT NULL        --게시글번호 : BOARD TABLE
+    , FILENAME  VARCHAR2(255) NOT NULL  --파일이름
+    , FILEEXT   VARCHAR2(255) NOT NULL  --파일확장자
+    , SFILENAME VARCHAR2(255) NOT NULL  --저장된실제파일명
     
+    , CONSTRAINTS FILES_PK PRIMARY KEY  --기본키(복합키)
+    (
+        FILE_NUM,
+        IDX
+    )
+    , CONSTRAINTS FK_BOARD_FILES_IDX
+    FOREIGN KEY (IDX)               --현재테이블의 칼럼명
+    REFERENCES  BOARD(IDX)
+    ON DELETE   CASCADE
+);
+
+SELECT
+    idx,
+    menu_id,
+    title,
+    writer,
+    (
+    SELECT COUNT(FILENAME) FROM FILES F
+    WHERE   B.IDX = F.IDX
+    ) filescount,    
+    regdate,
+    hit
+FROM
+    board   B
+WHERE
+    MENU_ID = 'MENU01'
+ORDER BY
+    IDX DESC
+    ;
